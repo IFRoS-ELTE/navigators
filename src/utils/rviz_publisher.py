488f7b45eadfcce8bed72e66dbbc5328c05c459b
Liviu Florescu <f_liviu@yahoo.com>
2024-11-06 19:43:11 +0100

    visualise pose history and axes

--- src/utils/rviz_publisher.py
+++ src/utils/rviz_publisher.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import rospy
 import tf
 from geometry_msgs.msg import (
@@ -9,8 +11,10 @@ from geometry_msgs.msg import (
     TransformStamped,
     Vector3,
 )
+from std_msgs.msg import ColorRGBA
 from utils.common import MAP_FRAME, POSE_TOPIC
 from utils.pose import Pose3D
+from visualization_msgs.msg import Marker, MarkerArray
 
 
 def header_map_frame_now():
@@ -23,6 +27,12 @@ class RVizPublisher:
 
         self.pose_pub = rospy.Publisher(POSE_TOPIC, PoseStamped, queue_size=1)
 
+        self.pose_history_pub = rospy.Publisher(
+            "pose_history", MarkerArray, queue_size=1
+        )
+
+        self.published_poses_max_id = 0
+
     def publish_pose(self, pose: Pose3D, timestamp):
         """Publish the pose in the map frame in RViz."""
         p = PoseStamped(header=header_map_frame_now())
@@ -34,3 +44,48 @@ class RVizPublisher:
         p.pose.orientation = Quaternion(*q)
 
         self.pose_pub.publish(p)
+
+    def publish_pose_list(self, pose_list: List[Pose3D]):
+        """Publish a marker for each pose."""
+
+        namespace = "pose_history"
+        purple = ColorRGBA(0.62, 0.35, 1, 1)
+        pub = self.pose_history_pub
+
+        # Delete all markers
+        deletion_markers = [
+            Marker(id=i, action=Marker.DELETE, ns=namespace)
+            for i in range(self.published_poses_max_id)
+        ]
+        pub.publish(MarkerArray(deletion_markers))
+
+        # Create new markers
+        pose_markers = [
+            pose2D_to_marker(p, id=i, ns=namespace, color=purple)
+            for i, p in enumerate(pose_list)
+        ]
+        pub.publish(MarkerArray(pose_markers))
+
+        self.published_poses_max_id = len(pose_list) - 1
+
+
+def pose3D_to_pose(p: Pose3D):
+    """Create a Pose object from a Pose3D object."""
+    pose = Pose()
+    pose.position.x = p.x
+    pose.position.y = p.y
+    quaternion = tf.transformations.quaternion_from_euler(0, 0, p.theta)
+    pose.orientation = Quaternion(*quaternion)
+    return pose
+
+
+def pose2D_to_marker(pose: Pose3D, **kwargs):
+    """Create a Marker from a Pose3D object."""
+    return Marker(
+        header=header_map_frame_now(),
+        action=Marker.ADD,
+        type=Marker.ARROW,
+        pose=pose3D_to_pose(pose),
+        scale=Vector3(0.5, 0.01, 0.01),
+        **kwargs,
+    )
